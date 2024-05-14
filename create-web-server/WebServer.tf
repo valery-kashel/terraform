ec9e7345cba02dfc13352a398255b6a6e2f1e271
Valery <valery.kashel@gmail.com>
2024-05-14 21:28:14 +0200

    use dynamic ingress in WebServer.tf

--- create-web-server/WebServer.tf
+++ create-web-server/WebServer.tf
@@ -16,18 +16,14 @@ resource "aws_security_group" "my_webserver" {
   name        = "WebServer Security Group"
   description = "My first Security Group"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = ["80", "443", "22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
